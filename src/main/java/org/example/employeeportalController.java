01c1351e36f85b72546396eb7d639100b813ae2e
SAI <bojedlasaiprasad@gmail.com>
2025-05-18 18:06:24 -0400

    Adding all the Existing departments to the Employee Portal

---
+++ src/main/java/org/example/employeeportalController.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class employeeportalController {
+    public static void main(String[] args) {
+
+    System.out.println("Employee Directory");
+    System.out.println("Adminstration DEPT");
+    System.out.println("Sales DEPT");
+    System.out.println("Operations DEPT");
+    System.out.println("Collections DEPT");
+    }
+}
